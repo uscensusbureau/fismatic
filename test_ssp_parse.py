6e3a1be09bb50694ad0a70a821057b0ca36df50a
Aidan Feldman <aidan.l.feldman@census.gov>
2019-05-17 15:30:35 -0400

    test that columns headings fully match the rows

--- test_ssp_parse.py
+++ test_ssp_parse.py
@@ -7,15 +7,18 @@ def test_matrix():
     source_doc = "Azure Security and Compliance Blueprint - FedRAMP High SSP.docx"
     ssp_parse.run(source_doc)
 
-    # check that the columns match the rows
     with open("matrix.csv") as csv_file:
         csv_reader = csv.DictReader(csv_file)
-        expected = ["AC-1: Part a", "AC-1: Part b", "AC-2: Part a", "AC-2: Part b"]
 
-        column_headings = csv_reader.fieldnames[1:5]
-        assert column_headings == expected
+        column_headings = csv_reader.fieldnames[1:]
+        assert column_headings[0:4] == [
+            "AC-1: Part a",
+            "AC-1: Part b",
+            "AC-2: Part a",
+            "AC-2: Part b",
+        ]
 
+        # check that the columns match the rows
         csv_reader = csv.reader(csv_file)
-        top_rows = itertools.islice(csv_reader, 4)
-        row_headings = [row[0] for row in top_rows]
-        assert row_headings == expected
+        row_headings = [row[0] for row in csv_reader]
+        assert row_headings == column_headings
