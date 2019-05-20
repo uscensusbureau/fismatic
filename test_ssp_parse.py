542f443ab0bf20a7bad5641050238a1060b58033
Aidan Feldman <aidan.l.feldman@census.gov>
2019-05-20 16:05:27 -0400

    add test around getting list of controls

--- test_ssp_parse.py
+++ test_ssp_parse.py
@@ -1,11 +1,13 @@
 import csv
+from docx import Document
 import itertools
 import ssp_parse
 
+SOURCE_DOC = "Azure Security and Compliance Blueprint - FedRAMP High SSP.docx"
+
 
 def test_matrix():
-    source_doc = "Azure Security and Compliance Blueprint - FedRAMP High SSP.docx"
-    ssp_parse.run(source_doc)
+    ssp_parse.run(SOURCE_DOC)
 
     with open("matrix.csv") as csv_file:
         csv_reader = csv.DictReader(csv_file)
@@ -22,3 +24,14 @@ def test_matrix():
         csv_reader = csv.reader(csv_file)
         row_headings = [row[0] for row in csv_reader]
         assert row_headings == column_headings
+
+
+def test_get_controls():
+    doc = Document(docx=SOURCE_DOC)
+    tables = ssp_parse.get_tables(doc)
+
+    controls = ssp_parse.get_controls(tables)
+
+    control_ids = list(controls.keys())
+    # TODO address inconsistent spacing
+    assert control_ids[0:4] == ["AC-1", "AC-2", "AC-2(1)", "AC-2 (2)"]
