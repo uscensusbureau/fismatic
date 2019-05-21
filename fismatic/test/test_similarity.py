546252ba9cd5ea51b6051f0b4de12f0ac6531da0
Aidan Feldman <aidan.l.feldman@census.gov>
2019-05-21 16:44:02 -0400

    test similar_controls()

--- fismatic/test/test_similarity.py
+++ fismatic/test/test_similarity.py
@@ -10,3 +10,16 @@ def test_generate_diffs():
         [1.0, 1.0, 0.0],  # Foo
         [0.0, 0.0, 1.0],  # bar
     ]
+
+
+def test_similar_controls():
+    desc_lkup = ["AC-1", "AC-2", "AC-2 (1)"]
+    diffs = [
+        # AC-1, AC-2, AC-2 (1)
+        [1.0, 0.9, 0.0],  # AC-1
+        [0.9, 1.0, 0.0],  # AC-2
+        [0.0, 0.0, 1.0],  # AC-2 (1)
+    ]
+
+    very_similar = similarity.similar_controls(desc_lkup, diffs)
+    assert very_similar == {"AC-1": {"AC-2": "0.9"}, "AC-2": {"AC-1": "0.9"}}
