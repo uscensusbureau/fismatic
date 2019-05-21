0c869715da1e893e5b5d45e91f38ee786d5e0a78
Aidan Feldman <aidan.l.feldman@census.gov>
2019-05-21 13:30:14 -0400

    test the parsing of the implementation text

--- fismatic/control.py
+++ fismatic/control.py
@@ -1,6 +1,7 @@
 class Control:
     def __init__(self):
         self.name = None
+        """Specific to FedRAMP templates"""
         self.responsible_role = None
         self.imp_status = None
         self.origination = None
--- fismatic/test/test_docx_parser.py
+++ fismatic/test/test_docx_parser.py
@@ -10,3 +10,13 @@ def test_get_controls():
     control_ids = list(controls.keys())
     # TODO address inconsistent spacing
     assert control_ids[0:4] == ["AC-1", "AC-2", "AC-2(1)", "AC-2 (2)"]
+
+
+def test_implementation():
+    parser = DocxParser(common.SOURCE_DOC)
+    controls = parser.get_controls()
+
+    control = controls["AC-1"]
+    implementation = control.implementation["Part a"]
+    assert "Microsoft Azure" in implementation
+    assert "customer is responsible" in implementation
