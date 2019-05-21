bdd0c02098aa489a4d85aadda45279cf826dc99a
Aidan Feldman <aidan.l.feldman@census.gov>
2019-05-21 15:50:26 -0400

    use pandas for matrix test

--- fismatic/test/test_core.py
+++ fismatic/test/test_core.py
@@ -1,4 +1,5 @@
 import csv
+import pandas as pd
 from . import common
 from .. import core as fismatic
 
@@ -6,18 +7,14 @@ from .. import core as fismatic
 def test_matrix():
     fismatic.run(common.SOURCE_DOC)
 
-    with open("matrix.csv") as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-
-        column_headings = csv_reader.fieldnames[1:]
-        assert column_headings[0:4] == [
-            "AC-1: Part a",
-            "AC-1: Part b",
-            "AC-2: Part a",
-            "AC-2: Part b",
-        ]
-
-        # check that the columns match the rows
-        csv_reader = csv.reader(csv_file)
-        row_headings = [row[0] for row in csv_reader]
-        assert row_headings == column_headings
+    df = pd.read_csv("matrix.csv", index_col=0)
+    column_names = df.columns
+    assert list(column_names[0:4]) == [
+        "AC-1: Part a",
+        "AC-1: Part b",
+        "AC-2: Part a",
+        "AC-2: Part b",
+    ]
+    # check that the columns match the rows
+    row_names = df.index
+    assert list(column_names) == list(row_names)
