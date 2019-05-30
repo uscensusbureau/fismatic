42b0bdd6c514c2ca6e1f2bc7ba593d758349b765
Aidan Feldman <aidan.l.feldman@census.gov>
2019-05-30 02:46:19 -0400

    eliminate redundant parses of the docx in tests

--- fismatic/test/test_docx_parser.py
+++ fismatic/test/test_docx_parser.py
@@ -1,20 +1,16 @@
 from . import common
 from ..docx_parser import DocxParser
 
+parser = DocxParser(common.SOURCE_DOC)
+controls = parser.get_controls()
+
 
 def test_get_controls():
-    parser = DocxParser(common.SOURCE_DOC)
-
-    controls = parser.get_controls()
-
     control_ids = list(controls.keys())
     assert control_ids[0:4] == ["AC-1", "AC-2", "AC-2 (1)", "AC-2 (2)"]
 
 
 def test_implementation():
-    parser = DocxParser(common.SOURCE_DOC)
-    controls = parser.get_controls()
-
     control = controls["AC-1"]
     implementation = control.implementation["Part a"]
     assert "Microsoft Azure" in implementation.text
