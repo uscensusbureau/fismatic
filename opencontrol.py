39f205404b3038b7f3e1390065bdef7d60ec1b13
Aidan Feldman <aidan.l.feldman@census.gov>
2019-06-04 14:18:36 -0400

    search GitHub for OpenControl Components

---
+++ opencontrol.py
@@ -0,0 +1,15 @@
+from github import Github
+import os
+
+token = os.getenv("GITHUB_TOKEN")
+g = Github(token)
+
+# The GitHub code search API requires one or more org/user to be specified.
+# https://developer.github.com/changes/2013-10-18-new-code-search-requirements/#new-validation-rule
+# The following is a curated list. To find more, go to:
+# https://github.com/search?utf8=%E2%9C%93&q=schema_version+satisfies+control_key+language%3Ayaml&type=Code
+users = ["docker", "m3brown", "opencontrol"]
+users_q = " ".join("user:{}".format(user) for user in users)
+results = g.search_code("schema_version satisfies control_key language:yaml " + users_q)
+for result in results:
+    print(result)
