164851248185695170f44f25f424ee5b4a102d1d
Aidan Feldman <aidan.l.feldman@census.gov>
2019-06-04 14:18:49 -0400

    expand the list of OpenControl repository sources

--- opencontrol.py
+++ opencontrol.py
@@ -8,7 +8,25 @@ g = Github(token)
 # https://developer.github.com/changes/2013-10-18-new-code-search-requirements/#new-validation-rule
 # The following is a curated list. To find more, go to:
 # https://github.com/search?utf8=%E2%9C%93&q=schema_version+satisfies+control_key+language%3Ayaml&type=Code
-users = ["docker", "m3brown", "opencontrol"]
+users = [
+    "18F",
+    "ComplianceAsCode",
+    "corbaltcode",
+    "docker",
+    "GovReady",
+    "GSA",
+    "jenglish",
+    "jmmcnj",
+    "m3brown",
+    "madhugilla",
+    "nsagoo-pivotal",
+    "opencontrol",
+    "redhatrises",
+    "SecurityCentral",
+    "shawndwells",
+    "superbrilliant",
+    "weirdscience",
+]
 users_q = " ".join("user:{}".format(user) for user in users)
 results = g.search_code("schema_version satisfies control_key language:yaml " + users_q)
 for result in results:
