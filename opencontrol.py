8d5fd2ca35260fd7a65f845d0849530dc35ea401
Aidan Feldman <aidan.l.feldman@census.gov>
2019-06-04 15:48:51 -0400

    print the list of OpenControl repositories

--- opencontrol.py
+++ opencontrol.py
@@ -1,6 +1,7 @@
-from github import Github
+from github import Github, RateLimitExceededException
 import os
 import requests_cache
+import sys
 
 requests_cache.install_cache("requests_cache")
 
@@ -32,5 +33,15 @@ users = [
 ]
 users_q = " ".join("user:{}".format(user) for user in users)
 results = g.search_code("schema_version satisfies control_key language:yaml " + users_q)
-for result in results:
-    print(result)
+
+# get as many as we can before hitting the rate limit
+repos = set()
+try:
+    for result in results:
+        repos.add(result.repository.full_name)
+except RateLimitExceededException as err:
+    print(err, file=sys.stderr)
+
+sorted_repos = list(repos)
+sorted_repos.sort()
+print(sorted_repos)
