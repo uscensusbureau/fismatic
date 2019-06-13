402369aa005fbd14d2d8da91043ac39870bd83fb
Aidan Feldman <aidan.l.feldman@census.gov>
2019-06-13 14:27:01 -0400

    add tests around similar_implementations()

---
+++ fismatic/test/test_demo.py
@@ -0,0 +1,34 @@
+from ..control import Control
+from ..control_set import ControlSet
+from ..demo import similar_implementations
+
+
+def test_similar_implementations_empty():
+    results = similar_implementations([], "foo", "a", "bar")
+    assert results == []
+
+
+def test_similar_implementations():
+    control1 = Control("AC-1")
+    control1.implementation = {"A": "Something else."}
+    control2 = Control("AC-2")
+    imp1 = "This is about computers."
+    control2.implementation = {"A": imp1}
+    control_set1 = ControlSet([control1, control2])
+
+    control3 = Control("AC-2")
+    imp2 = "This is also about computers. Computers do a lot."
+    control3.implementation = {"A": imp2}
+    control_set2 = ControlSet([control3])
+
+    control4 = Control("AC-2")
+    imp3 = "Irrelevant."
+    control4.implementation = {"A": imp3}
+    control_set3 = ControlSet([control4])
+
+    control_sets = [control_set1, control_set2, control_set3]
+
+    results = similar_implementations(control_sets, "AC-2", "A", "computers")
+    result_txt = [imp.text for imp in results]
+    assert result_txt == [imp2, imp1, imp3]
+
