c0486aa8e561c2f261f06bc8746d1f721547a123
Aidan Feldman <aidan.l.feldman@census.gov>
2019-06-13 14:38:08 -0400

    run all notebooks as part of tests

--- fismatic/test/test_notebook.py
+++ fismatic/test/test_notebook.py
@@ -1,10 +1,16 @@
+import glob
 import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
 
 
-def test_notebook():
-    # https://nbconvert.readthedocs.io/en/latest/execute_api.html#executing-notebooks-using-the-python-api-interface
-    with open("./analysis.ipynb") as f:
-        nb = nbformat.read(f, as_version=4)
-        ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
-        ep.preprocess(nb, {})
+def test_notebooks():
+    notebooks = glob.glob("./*.ipynb")
+    # safety check
+    assert len(notebooks) > 2
+
+    for notebook in notebooks:
+        # https://nbconvert.readthedocs.io/en/latest/execute_api.html#executing-notebooks-using-the-python-api-interface
+        with open(notebook) as f:
+            nb = nbformat.read(f, as_version=4)
+            ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
+            ep.preprocess(nb, {})
